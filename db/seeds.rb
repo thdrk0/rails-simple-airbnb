13f01cc8e04a05047314466f5b2c37d92500a532
theodorik comlan <comlantheodorik@gmail.com>
2024-05-22 03:41:31 +0200

    simple airbnb not finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+10.times do
+  flat = Flat.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.fortune_cookie,
+    price_per_night: Faker::Number.between(from: 100, to: 10_000),
+    number_of_guests: Faker::Number.between(from: 1, to: 10)
+  )
+  flat.save
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
